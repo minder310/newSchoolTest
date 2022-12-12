6b12e8c7011e44a2888f69fb0335b86fcf0f2492
Pierre.K <minder309@gmail.com>
2022-12-12 19:18:23 +0800

    db/base.php 建立亞洲時間，並且宣告資料庫取用語法，與帳號密碼。     index.php 帶入資料庫語法，帶入bts。     layouts/link_css.php 宣告bts並且，用include，帶入index。

---
+++ db/base.php
@@ -0,0 +1,16 @@
+<?php
+// 宣告mysql語法，並且宣告要連結的資料庫。
+$dns="mysql:host=localhost;charset=utf8;dbname=school";
+// 宣告pdo=資料庫資料並匯入，上面語法，並且宣告，帳號密碼。
+$pdo=new PDO($dns,'root','');
+
+// 宣告時間格式，並設定亞洲台北。
+date_default_timezone_set("Asia/Taipei");
+
+// 用來顯示陣列內容。並且可以快速輸出，基本上就是懶。
+function dd($array){
+    echo "<pre>";
+    print_r($array);
+    echo "</pre>";
+}
+?>
---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+include "./db/base.php";
+// session_start()取用資料庫寫法皆寫在include中(include等同複製貼上，將每個程式碼分開。)
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>學生管理系統</title>
+    <?php include "./layouts/link_css.php";?>
+    <!-- 導入bts資料也是用include資料導入，複製貼上(純粹增加印象。) -->
+    <link rel="stylesheet" href="./css/style.css">
+    <!-- 這一段是導入css -->
+</head>
+<body>
+    
+</body>
+</html>
