c28d152152e3f08b1acaad061bd317b6584af451
minder309 <minder309@gmil.com>
2022-12-13 12:52:44 +0800

    後端api登入系統。建至完成。     前端登入頁面，建置完成。

---
+++ api/chk_user.php
@@ -0,0 +1,45 @@
+<?php
+include "./db/base.php";
+// 導入資料庫檔案。
+
+session_start();
+// 宣告session可以使用。並且可以傳值。
+
+//導入帳號密碼用post的方式傳值。
+$acc=$_POST['acc'];
+$pw=$_POST['pw'];
+
+$sql="select count(`id`) from `users` where `acc`='$acc' && `pw`='$pw'";
+// 顯示users表單中，acc==acc和pw==pw值得id。 要記得這是語法宣告，下面才會帶進資料庫中。
+$chk=$pdo->query($sql)->fetchColumn();
+// fetchColumn()回傳一列，如果有多列，只會顯示最上面的一列，如果沒有則回傳回false。
+// 這邊在我看來是用來宣告，是否有這個帳號密碼用的初階。
+
+// 回傳值==1等於有這個帳號密碼。
+if($chk==1){
+    $sql="select `id`,`acc`,`name`,`last_login` from `users` where `acc`='$acc' && `pw`='$pw'";
+    // 從users取出id acc name last_login條件是acc==acc和pw==pw。
+    $user=$pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
+    // 取出使用者資料，並且將login=user資料。
+    dd($user);
+    $_SESSION['login']=$user;
+
+}else{
+    if(isset($_SESSION['login_try'])){
+        $_SESSION['login_try']++;
+    }else{
+        $_SESSION['login_try']=1;
+    }
+}
+echo "這邊沒有加header，所以不會自動跳回去。"
+
+
+
+
+
+
+
+
+
+
+?>
---
+++ front/login.php
@@ -0,0 +1,28 @@
+<!-- 登入頁面，製作 -->
+<div class="col-md-4 mx-auto my-5 p-5 border shadow-sm">
+    <h3 class="text-center">教師登入</h3>
+    <?php
+    // 進入登入頁面，回傳值為error，即顯示登入錯誤幾次，並且顯示。
+    if(isset($_GET['error']))
+    {
+        echo "<div class='text-danger text-center'>";
+        echo "帳號密碼登入錯誤,";
+        echo "登入嘗試".$_SESSION['login_try']."次";
+        echo "</div>";
+    }
+    ?>
+        <!-- 將帳號密碼，傳值至../api/chk_user.php -->
+    <form action="../api/chk_user.php" method="post">
+        <div class="form-group">
+            <label>帳號</label>
+            <input type="text" class="form-control" name="acc" id="">
+        </div>
+        <div class="form-group">
+            <label>密碼</label>
+            <input type="text" class="form-control" type="password" name="pw" id="">
+        </div>
+        <div class="text-center">
+            <input type="text" class="btn btn-primary" type="subimit" value="登入" id="">
+        </div>
+    </form>
+</div>
